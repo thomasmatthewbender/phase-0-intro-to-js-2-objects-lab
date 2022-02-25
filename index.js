f35bbd7b24d4e349e7d4a6d475f02a5f27eba1a8
Thomas Bender <thomasmatthewbender@gmail.com>
2022-02-25 12:32:40 -0500

    commit tree 5e2fbb2d92ccd200b83cd5701194a320d4f482f7

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "John Doe", 
+    streetAddress: "4325 Congress Street"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employeeObjNew = {...employee}
+
+    employeeObjNew[key] = value
+
+    return employeeObjNew
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeObjNew = {...employee}
+
+    delete employeeObjNew[key]
+
+    return employeeObjNew
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
